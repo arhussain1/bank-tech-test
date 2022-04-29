1ce9a435bdd3e223101b04fb31605c8d478d8409
arhussain1 <a.r.hussain@hotmail.com>
2022-04-29 02:33:10 +0100

    extracted formatting logic for creating statement body from createStatement

--- statement.js
+++ statement.js
@@ -4,23 +4,23 @@ class Statement {
 
   createStatement(activityLog) {
     const header = ['date || credit || debit || balance']
-    let body = []
-
-    activityLog.forEach(transaction => {
-      const balance = this.#formatMoney(transaction.balance)
-      const credit = this.#formatMoney(transaction.credit)
-      const debit = this.#formatMoney(transaction.debit)
-
-      const string = `${transaction.date} || ${credit} || ${debit} || ${balance}`
-      body.push(string);
-    })
+    const body = this.#createStatementBody(activityLog)
 
     const organisedArray = header.concat(body.reverse())
-
     return organisedArray
   }
 
   // private methods
+  #createStatementBody(activityLog) {
+    const body = activityLog.map(transaction => {
+      const balance = this.#formatMoney(transaction.balance)
+      const credit = this.#formatMoney(transaction.credit)
+      const debit = this.#formatMoney(transaction.debit)
+
+      return `${transaction.date} || ${credit} || ${debit} || ${balance}`
+    })
+    return body
+  }
 
   #formatMoney(num) {
     if (typeof num == "number") return num.toFixed(2)
